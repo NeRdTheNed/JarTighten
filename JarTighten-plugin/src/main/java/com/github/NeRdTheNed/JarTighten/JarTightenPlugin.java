acc75d0f502c6be007a53d5d87789cb77f2357b7
Ned Loynd <ned@loynd.com>
2023-07-03 11:35:42 +1000

    Only use the jar task if it exists

--- JarTighten-plugin/src/main/java/com/github/NeRdTheNed/JarTighten/JarTightenPlugin.java
+++ JarTighten-plugin/src/main/java/com/github/NeRdTheNed/JarTighten/JarTightenPlugin.java
@@ -2,18 +2,21 @@ package com.github.NeRdTheNed.JarTighten;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.jvm.tasks.Jar;
 
 public class JarTightenPlugin implements Plugin<Project> {
     @Override
-    public void apply(Project target) {
-        final Jar jarTask = ((Jar) target.getTasks().getByName("jar"));
-        final JarTightenTask jarTightenTask = target.getTasks().create("jartighten", JarTightenTask.class);
-        jarTightenTask.getRecompress().convention(false);
-        jarTightenTask.getRemoveTimestamps().convention(false);
-        jarTightenTask.getRemoveFileLength().convention(false);
-        jarTightenTask.getRemoveFileNames().convention(false);
-        jarTightenTask.getInputFile().convention(jarTask.getArchiveFile());
-        jarTightenTask.getOutputFile().convention(jarTask.getArchiveFile());
+    public void apply(Project project) {
+        if (project.getPlugins().hasPlugin(JavaPlugin.class)) {
+            final Jar jarTask = (Jar) project.getTasks().getByName("jar");
+            final JarTightenTask jarTightenTask = project.getTasks().create("jartighten", JarTightenTask.class);
+            jarTightenTask.getRecompress().convention(false);
+            jarTightenTask.getRemoveTimestamps().convention(false);
+            jarTightenTask.getRemoveFileLength().convention(false);
+            jarTightenTask.getRemoveFileNames().convention(false);
+            jarTightenTask.getInputFile().convention(jarTask.getArchiveFile());
+            jarTightenTask.getOutputFile().convention(jarTask.getArchiveFile());
+        }
     }
 }
