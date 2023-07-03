a17d6760055e6ab942942e646ac4889747e4a09e
Ned Loynd <ned@loynd.com>
2023-07-03 11:12:38 +1000

    Allow configuring more properties

--- JarTighten-plugin/src/main/java/com/github/NeRdTheNed/JarTighten/JarTightenPlugin.java
+++ JarTighten-plugin/src/main/java/com/github/NeRdTheNed/JarTighten/JarTightenPlugin.java
@@ -5,12 +5,15 @@ import org.gradle.api.Project;
 import org.gradle.jvm.tasks.Jar;
 
 public class JarTightenPlugin implements Plugin<Project> {
-
     @Override
     public void apply(Project target) {
         final Jar jarTask = ((Jar) target.getTasks().getByName("jar"));
         final JarTightenTask jarTightenTask = target.getTasks().create("jartighten", JarTightenTask.class);
-        jarTightenTask.getInputFile().set(jarTask.getArchiveFile());
-        jarTightenTask.getOutputFile().set(jarTask.getArchiveFile());
+        jarTightenTask.getRecompress().convention(false);
+        jarTightenTask.getRemoveTimestamps().convention(false);
+        jarTightenTask.getRemoveFileLength().convention(false);
+        jarTightenTask.getRemoveFileNames().convention(false);
+        jarTightenTask.getInputFile().convention(jarTask.getArchiveFile());
+        jarTightenTask.getOutputFile().convention(jarTask.getArchiveFile());
     }
 }
--- JarTighten-plugin/src/main/java/com/github/NeRdTheNed/JarTighten/JarTightenTask.java
+++ JarTighten-plugin/src/main/java/com/github/NeRdTheNed/JarTighten/JarTightenTask.java
@@ -6,7 +6,11 @@ import java.util.List;
 
 import org.gradle.api.DefaultTask;
 import org.gradle.api.file.RegularFileProperty;
+import org.gradle.api.provider.ListProperty;
+import org.gradle.api.provider.Property;
+import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputFile;
+import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
 
@@ -17,20 +21,35 @@ public abstract class JarTightenTask extends DefaultTask {
     @OutputFile
     public abstract RegularFileProperty getOutputFile();
 
+    @Input
+    @Optional
+    public abstract Property<Boolean> getRecompress();
+
+    @Input
+    @Optional
+    public abstract Property<Boolean> getRemoveTimestamps();
+
+    @Input
+    @Optional
+    public abstract Property<Boolean> getRemoveFileLength();
+
+    @Input
+    @Optional
+    public abstract Property<Boolean> getRemoveFileNames();
+
+    @Input
+    @Optional
+    public abstract ListProperty<String> getExcludes();
+
     @TaskAction
     public void jarTighten() throws Exception {
         final Path inputPath = getInputFile().getAsFile().get().toPath();
         final Path outputPath = getOutputFile().getAsFile().get().toPath();
-        // TODO
-        final List<String> excludes = new ArrayList<>();
-        // TODO
-        final boolean removeTimestamps = false;
-        // TODO
-        final boolean removeFileLength = false;
-        // TODO
-        final boolean removeFileNames = false;
-        // TODO
-        final boolean recompress = false;
+        final List<String> excludes = getExcludes().getOrNull();
+        final boolean removeTimestamps = getRemoveTimestamps().getOrElse(false);
+        final boolean removeFileLength = getRemoveFileLength().getOrElse(false);
+        final boolean removeFileNames = getRemoveFileNames().getOrElse(false);
+        final boolean recompress = getRecompress().getOrElse(false);
         JarTighten.optimiseJar(inputPath, outputPath, true, excludes != null ? excludes : new ArrayList<String>(), removeTimestamps, removeFileLength, removeFileNames, recompress);
     }
 }
